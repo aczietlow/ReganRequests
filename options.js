49b26ce91bf8cbb8a034da0f30d7d715db0aa7cf
Chris Zietlow <aczietlow@gmail.com>
2018-08-08 12:27:31 -0400

    Adding base options page.

---
+++ options.js
@@ -0,0 +1,22 @@
+function saveOptions(e) {
+    browser.storage.sync.set({
+        it_email: document.querySelector("#it_email").value,
+        email: document.querySelector("#email").value
+    });
+    e.preventDefault();
+}
+
+function restoreOptions() {
+    var storageItem = browser.storage.managed.get('it_email');
+    storageItem.then((res) => {
+        document.querySelector("#managed-colour").innerText = res.it_email;
+    });
+
+    var gettingItem = browser.storage.sync.get('it_email');
+    gettingItem.then((res) => {
+        document.querySelector("#it_email").value = res.it_email || '<help@me.com>';
+    });
+}
+
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.querySelector("form").addEventListener("submit", saveOptions);
